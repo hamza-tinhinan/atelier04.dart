10c6d9ab48118aad239577b7bbb5bd25e15e0a95
adel bennacer <adelbennacer46gmail.com>
2025-11-13 16:31:18 -0500

    ajouter une methode display

--- exercice02.dart
+++ exercice02.dart
@@ -2,4 +2,13 @@ class Point {
   double _x, _y;
 
   Point(this._x, this._y);
+  
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+  void display() {
+    print("Point(x: $_x, y: $_y)");
+  }
 }
