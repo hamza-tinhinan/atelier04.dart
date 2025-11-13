e527e59a22c345a7dbd72979d61756287038be02
adel bennacer <adelbennacer46gmail.com>
2025-11-13 16:05:01 -0500

    cree une methode public

--- exercice01.dart
+++ exercice01.dart
@@ -1,4 +1,15 @@
 class Couleur {
   final int r, g, b;
 
-  Couleur(this.r, this.g, this.b);}
+  Couleur(this.r, this.g, this.b);
+
+
+  
+  Couleur.rouge() : this(255, 0, 0);
+  Couleur.vert() : this(0, 255, 0);
+  Couleur.bleu() : this(0, 0, 255);
+
+  void afficherCouleur() {
+    print("R: $r, V: $g, B: $b");
+  }
+}
