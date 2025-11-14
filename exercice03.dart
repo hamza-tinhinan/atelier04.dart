4bfacc5a2f9cade3e92e295f41959999e5a66f52
adel bennacer <adelbennacer46gmail.com>
2025-11-13 20:39:24 -0500

    implimenter une methode complete

--- exercice03.dart
+++ exercice03.dart
@@ -4,4 +4,13 @@ class Compte {
 
   Compte(this.numeroCompte, [this._solde = 0.0]);
   String get solde => "${_solde.toStringAsFixed(2)} \$";
+  void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      print("Dépôt de $montant \$ effectué avec succès !");
+    } else {
+      print("Le montant doit être positif.");
+    }
   }
+}
+  
