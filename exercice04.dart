cc932806aea9bb3c2c161dd793e2a5779727fba5
adel bennacer <adelbennacer46gmail.com>
2025-11-13 21:07:48 -0500

    redifinner la methode

---
+++ exercice04.dart
@@ -0,0 +1,18 @@
+class Media {
+  final String titre;
+  Media(this.titre);
+
+  void afficherType() {
+    print("Ceci est un média générique.");
+  }
+}
+
+class Livre extends Media {
+  final String auteur;
+  Livre(String titre, this.auteur) : super(titre);
+
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : '$titre' par $auteur.");
+  }
+}
