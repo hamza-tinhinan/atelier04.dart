6519e2cf2e540e55d38608371675b7f5411a9ccc
adel bennacer <adelbennacer46gmail.com>
2025-11-14 09:50:37 -0500

    cree 3 instances de taches

--- exercice07.dart
+++ exercice07.dart
@@ -6,4 +6,10 @@ Tache(this.description) {
   nombreTotal++;
    }
 } 
+void main () {
+  var t1 = Tache("mange une pomme");
+  var t2 = Tache("Faire du sport");
+  var t3 = Tache("joue du foot");
+   print("Nombre total de tâches : ${Tache.nombreTotal}");
+  }
   
